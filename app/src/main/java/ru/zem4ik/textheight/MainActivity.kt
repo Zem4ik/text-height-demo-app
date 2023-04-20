ab1a185c982236b08f192d9d67532c5d74998de2
Zem4ik <vladbullzv@gmail.com>
2023-04-20 18:22:37 +0300

    Скелет проекта

---
+++ app/src/main/java/ru/zem4ik/textheight/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.zem4ik.textheight
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
